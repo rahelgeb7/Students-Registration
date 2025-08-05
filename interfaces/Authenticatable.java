d02396cbc50319a6a8c2124825191871a966b0c2
matbits116 <matiyas.shiferaw@bitscollege.edu.et>
2025-08-05 10:59:42 +0300

    model and interfaces

---
+++ interfaces/Authenticatable.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import model.User;
+
+public interface Authenticatable {
+
+    boolean authenticate(String username, String password);
+
+    void registerUser(String username, String password, String role);
+
+    void changePassword(String username, String oldPassword, String newPassword);
+    
+}
+
